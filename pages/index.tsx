a684972b0e64de7d5ff07233da19f5137e378af4
CristianRod21 <cjrs21@hotmail.com>
2023-03-05 19:04:07 -0600

    add mui and migrate from /app to pages

---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '../src/Link';
+
+export default function Home() {
+  return (
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          my: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h4" component="h1" gutterBottom>
+          Portfolio
+        </Typography>
+        <Link href="/" color="secondary">
+            Go to the main page
+        </Link>
+      </Box>
+    </Container>
+  );
+}
